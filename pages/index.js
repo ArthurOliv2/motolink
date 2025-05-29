9dee7e8b2d3ba8dd042a9d73f014691d660b2669
ArthurOliv2 <arthuroliveirap2014@gmail.com>
2025-05-29 12:49:32 -0300

    Utiliza Cookie para autorização

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,35 @@
+import { getCookie } from "cookies-next"
+import { useEffect } from "react"
+import { verifica } from "../services/user"
+import Error from "next/error"
+import { redirect } from "next/dist/server/api-utils"
+
 export default function Home() {
   return (
     <div>
         Página do usuario 
     </div>
   )
+}
+
+export const getServerSideProps = async ({ req, res }) => {
+    try {
+        const token = getCookie('authorization', { req, res })
+        if (!token) throw new Error('Token Inválido')
+
+        verifica(token)
+        return {
+            props: {}
+        }
+    } catch (err) {
+        return {
+            redirect: {
+                permanent: false,
+                destination: '/login'
+            },
+            props: {}
+        }
+
+    }
+
 }
--- pages/login.js
+++ pages/login.js
@@ -1,4 +1,7 @@
 import Link from 'next/link'
+import { setCookie } from 'cookies-next'
+import { useRouter } from 'next/router'
+import { useState } from 'react'
 import Image from 'next/image'
 import styles from '../styles/login.module.css'
 
@@ -7,16 +10,50 @@ import Input from '../src/components/input/input'
 import Button from '../src/components/button/button'
 
 export default function LoginPage(){
+    const [formData, setFormData] = useState({
+        email: '',
+        password: '',
+    })
+    const [error, setError] = useState('')
+    const router = useRouter()
+
+    const handleFormEdit = (event, name) => {
+        setFormData({
+            ...formData,
+            [name]: event.target.value
+            
+        })
+    }
+
+    const handleForm = async (event) => {
+        try {
+            event.preventDefault()
+            const response = await fetch(`/api/user/login`, {
+                method: 'POST',
+                body: JSON.stringify(formData)
+            })
+
+            const json = await response.json()
+            if (response.status !== 200) throw new Error(json)
+
+            setCookie('authorization', json)
+            router.push('/')
+        } catch (err) {
+            setError(err.message)          
+        }
+    }
+
     return (
         <div className={styles.background}> 
             
             <LoginCard title="Entre sua conta">
                 <Image src="/motorbike.png" width={80} height={80} style={{ filter: 'invert(1)' }}/>
-                <form className={styles.form}>
-                    <Input type="email" placeholder="Seu e-mail" />
-                    <Input type="password" placeholder="Sua Senha" />
+                <form className={styles.form} onSubmit={handleForm}>
+                    <Input type="email" placeholder="Seu e-mail" value={formData.email} required onChange={(e) => {handleFormEdit(e, 'email')}}/>
+                    <Input type="password" placeholder="Sua Senha" value={formData.password} required onChange={(e) => {handleFormEdit(e, 'password')}}/>
                     <Button>Entrar</Button>
                     <Link href="/cadastro">Ainda não possui conta ?</Link>
+                    {error && <p className={styles.error}>{error}</p>}
                 </form>
             </LoginCard>
         </div>
